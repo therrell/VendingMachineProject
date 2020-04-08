0de032c19cbdc07cac8df501af5f013437bad8c4
Tom <tommyherrell1@gmail.com>
2020-04-08 15:12:27 -0400

    Merged CRUD

--- machines/views.py
+++ machines/views.py
@@ -37,3 +37,43 @@ def Takes(request):
 
 def Includes(request):
     return HttpResponse("This is the Includes page")
+
+def addCRN(request):
+    if request.method == "POST":
+        crn = CRN(request.POST)
+        with connection.cursor() as cursor:
+            # need to update "request.crn.crnid" wrt frontend
+            cursor.execute("SELECT * FROM CRN WHERE CRN.CrnId = %s", [str(request.crn.crnid)])
+            row = cursor.fetchall()
+            # if not exist
+            if len(row) == 0:
+                cursor.execute("INSERT INTO CRN(CrnId, Subject, Number, BuildingId) VALUES(%s, %s, %s, %s)", [request.crn.id, crn.data['Subject'], crn.data['Number'], crn.data['BuildingId']])
+
+    # probably need to return a render or redirect to some url
+
+def deleteCRN(request):
+    with connection.cursor() as cursor:
+        # need to update "request.crn.crnid" wrt frontend
+        cursor.execute("SELECT CrnId FROM CRN WHERE CRN.CrnId = %s", [str(request.crn.crnid)])
+        row = cursor.fetchall()
+        cursor.execute("DELETE FROM CRN WHERE CRN.CrnId = %s", [row[0]])
+        cursor.execute("DELETE FROM Takes WHERE Takes.CrnId = %s", [row[0]])
+
+    # probably need to return a render or redirect to some url
+
+def updateCRN(request):
+    if request.method == "POST":
+        crn = CRN(request.POST)
+        with connection.cursor() as cursor:
+            # need to update "request.crn.crnid" wrt frontend
+            cursor.execute("SELECT CrnId FROM CRN WHERE CRN.CrnId = %s", [str(request.crn.crnid)])
+            row = cursor.fetchall()
+            if len(row) == 0:
+                print("CRN does not exist. Please add a CRN.")
+            else:
+                if crn.data['Subject']:
+                    cursor.execute("UPDATE CRN SET CRN.Subject = %s WHERE CRN.CrnId = %s", [crn.data['Subject'], row[0]])
+                if crn.data['Number']:
+                    cursor.execute("UPDATE CRN SET CRN.Number = %s WHERE CRN.CrnId = %s", [crn.data['Number'], row[0]])
+                if crn.data['BuildingId']:
+                    cursor.execute("UPDATE CRN SET CRN.BuildingId = %s WHERE CRN.CrnId = %s", [crn.data['BuildingId'], row[0]])
