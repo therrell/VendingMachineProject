6b5fd38263f853c207cd44b7f4036e64e5bf63b5
therrell <tommyherrell1@gmail.com>
2020-05-03 13:19:25 -0400

    removed unnecessary url

--- vendingmachine/urls.py
+++ vendingmachine/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .router import router
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('token-auth/', obtain_jwt_token),
     path('user/', include('user.urls')),
     ]
