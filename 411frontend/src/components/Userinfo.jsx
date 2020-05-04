fdee7a2281b5de3cf1816c65f4a0508396981c92
therrell <tommyherrell1@gmail.com>
2020-05-04 17:55:39 -0400

    working on products

--- 411frontend/src/components/Userinfo.jsx
+++ 411frontend/src/components/Userinfo.jsx
@@ -411,7 +411,7 @@ class Userinfo extends Component {
                       {row.crnID}
                     </TableCell>
                     <TableCell align="right">
-                      <EditModal data={row.crnID} editItem={this.editCRN} deleteItem={this.deleteCRN} newName={this.state.newCRN} newSubj={this.state.newSubj} newNum={this.state.newNum} newBuildId={this.state.newBuildId} handleChange={this.onChangeCRN} handleChange_subj={this.onChangeCRN_subj} handleChange_num={this.onChangeCRN_num} handleChange_build={this.onChangeCRN_build} />
+                      <EditModal data={row.id} addItem={this.addCRN} deleteItem={this.deleteCRN} newName={this.state.newCRN} newSubj={this.state.newSubj} newNum={this.state.newNum} newBuildId={this.state.newBuildId} handleChange={this.onChangeCRN} handleChange_subj={this.onChangeCRN_subj} handleChange_num={this.onChangeCRN_num} handleChange_build={this.onChangeCRN_build} />
                     </TableCell>
                     <TableCell align="left">
                       <Deletemodal data={row.id} deleteItem={this.deleteCRN} />
